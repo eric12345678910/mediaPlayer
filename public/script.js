6201b66bbd2898b2ddb0a82a85153caf5dbe1de3
eric12345678910 <ericlaudrum@gmail.com>
2024-09-02 19:53:11 -0400

    add comments and css

--- public/script.js
+++ public/script.js
@@ -1,18 +1,23 @@
+// dummy list
+let tracks = ['track 1', 'track 2', 'track 3', 'track 4', 'track 5', 'track 6'];
 
+// add file name to
 function addFile(text) {
     const ul = document.getElementById('track');
-    const li = document.createElement('li');
-    li.textContent = text;
-    ul.appendChild(li);
-}
 
+    for(let i=0; i < text.length; i++){
+        const li = document.createElement('li');
+        li.textContent = text[i];
+        ul.appendChild(li);
+    };
+};
 
+// perform functions
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOMContentLoaded');
-    addFile('track one');
-    addFile('track two');
-    addFile('track three');
+
+    // functions
+    addFile(tracks);
 });
 
 
-console.log('this is a test')
