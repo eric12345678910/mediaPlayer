cfab4a17ff84e43d9844bd38b3cd31df572dc361
eric12345678910 <ericlaudrum@gmail.com>
2024-09-02 21:06:31 -0400

    remove .DS_Store from file list

--- public/script.js
+++ public/script.js
@@ -8,12 +8,20 @@ fetch('http://localhost:3000/files')
     })
     .then(files => {
         const ul = document.getElementById('track');
-        const regex = /^eg - (.*).wav$/;
+        const regex = /^eg - (.*)\.wav$/;
 
         files.forEach(file => {
+            if(file === '.DS_Store'){
+                return;
+            }
             const match = file.match(regex);
             const li = document.createElement('li');
             li.textContent = match ? match[1]: file;
+
+            // add click event
+            li.addEventListener('click', () => {
+                console.log('song selected:', li.textContent);
+            });
             ul.appendChild(li);
         });
     })
