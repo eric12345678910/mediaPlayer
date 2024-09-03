5f9a9fd6a0eb88e8b84f0be1f61c01fc37535f24
eric12345678910 <ericlaudrum@gmail.com>
2024-09-02 20:27:22 -0400

    output directory contents to ul elements

--- node.js
+++ node.js
@@ -3,17 +3,28 @@ const fs = require('fs');
 // Import express from npm
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.get('/files', (req, res) => {
+    const directoryPath = path.join(__dirname, 'public');
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'Unable to read directory' });
+        }
+        res.json(files); // Send files list as JSON
+    });
+});
+
 app.listen(port, () => {
     console.log('Server is running on http://localhost:' + port);
 });
-
--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // dummy list
-let tracks = ['track 1', 'track 2', 'track 3', 'track 4', 'track 5', 'track 6'];
+//let tracks = ['track 1', 'track 2', 'track 3', 'track 4', 'track 5', 'track 6'];
 
 // add file name to
 function addFile(text) {
@@ -20,4 +20,21 @@ document.addEventListener('DOMContentLoaded', function() {
     addFile(tracks);
 });
 
-
+fetch('http://localhost:3000/files')
+    .then(response => {
+        if(!response.ok){
+            throw new Error('network response not ok');
+        }
+        return response.json();
+    })
+    .then(files => {
+        const ul = document.getElementById('track');
+        files.forEach(file => {
+            const li = document.createElement('li');
+            li.textContent = file;
+            ul.appendChild(li);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    });
